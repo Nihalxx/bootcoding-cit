1d8491eaf9310928e1274b0f55afa658f18dd23f
Nihalxx <bawankulenihal98@gmail.com>
2023-08-17 15:55:16 +0530

    nihal jawa

---
+++ java/nw.java
@@ -0,0 +1,12 @@
+public class nw {
+    public static void main(String[] args) {
+        int a=10;
+        int b=a++ + 10;
+        System.out.println(b);
+
+
+
+
+
+    }
+}
---
+++ java/pro.java
@@ -0,0 +1,6 @@
+public class pro {
+    public static void main(String[] args) {
+        System.out.println("hello world in jawa" );
+
+    }
+}
